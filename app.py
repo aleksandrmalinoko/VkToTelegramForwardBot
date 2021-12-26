3fcbcc115fde9bfd9a363159b972b0c3fb142269
aleksandr malinko <aleksandrmalinkodev@gmail.com>
2021-12-27 00:02:04 +0300

    Пересылает записи из вк в телеграм

--- app.py
+++ app.py
@@ -1,34 +1,43 @@
-import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+import vk_api.vk_api
+from vk_api.bot_longpoll import VkBotLongPoll
+from vk_api.bot_longpoll import VkBotEventType
+import telebot
+from telebot import types
+import configparser
 
-vk_session = vk_api.VkApi(token='c45b8a3ca1b40d4eba082ebf68b694871b67702dafa5fb264424e42c1a87b9721b1b39ccc796711a60697')
-longpoll = VkLongPoll(vk_session)
-vk = vk_session.get_api()
+config = configparser.ConfigParser()
+config.read('config.ini')
+vk_api_token = config['vk']['vk_api_token']
+telegram_api_token = config['telegram']['telegram_api_token']
+telegram_chat_id = config['telegram']['telegram_chat_id']
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-        if event.text == 'Первый вариант фразы' or event.text == 'Второй вариант фразы':  # Если написали заданную фразу
-            if event.from_user:  # Если написали в ЛС
-                vk.messages.send(  # Отправляем сообщение
-                    user_id=event.user_id,
-                    message='Ваш текст',
-                    random_id=0
-                )
-            elif event.from_chat:  # Если написали в Беседе
-                vk.messages.send(  # Отправляем собщение
-                    chat_id=event.chat_id,
-                    message='Ваш текст',
-                    random_id=0
-                )
-    elif event.type == VkEventType.MESSAGE_EDIT:
-        vk.messages.send(  # Отправляем сообщение
-            user_id=event.user_id,
-            message='Изменил сообщение',
-            random_id=0
-        )
-    elif event.type == VkEventType.USER_TYPING:
-        vk.messages.send(  # Отправляем сообщение
-            user_id=event.user_id,
-            message='Че печатаешь?',
-            random_id=0
-        )
+class VkServer:
+    def __init__(self, api_token, group_id, server_name: str = "Empty"):
+        self.server_name = server_name
+        self.vk = vk_api.VkApi(token=api_token)
+        self.long_poll = VkBotLongPoll(self.vk, group_id)
+        self.vk_api = self.vk.get_api()
+        self.telegram_bot = telebot.TeleBot(token=telegram_api_token)
+
+    def start(self):
+        for event in self.long_poll.listen():
+            if event.type == VkBotEventType.WALL_POST_NEW:
+                try:
+                    post_text = event.obj.text
+                except:
+                    continue
+                try:
+                    attachments = event.obj.attachments
+                    medias = []
+                    for attachment in attachments:
+                        photo_url = attachment['photo']['sizes'][-1]['url']
+                        medias.append(types.InputMediaPhoto(photo_url, post_text))
+                        post_text = ''
+                    self.telegram_bot.send_media_group(telegram_chat_id, medias)
+                except:
+                    self.telegram_bot.send_message(telegram_chat_id, post_text)
+
+
+if __name__ == '__main__':
+    vk_server = VkServer(vk_api_token, 179778319, "server1")
+    vk_server.start()
