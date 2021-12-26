4241f3694ea0c618e440b5be09d8f79cf490c472
aleksandr malinko <aleksandrmalinkodev@gmail.com>
2021-12-26 18:14:10 +0300

    Отвечает на сообщения пользователей

--- app.py
+++ app.py
@@ -1,18 +1,34 @@
-from flask import Flask, json, request
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
 
-app = Flask(__name__)
+vk_session = vk_api.VkApi(token='c45b8a3ca1b40d4eba082ebf68b694871b67702dafa5fb264424e42c1a87b9721b1b39ccc796711a60697')
+longpoll = VkLongPoll(vk_session)
+vk = vk_session.get_api()
 
-
-@app.route('/', methods=['POST'])
-def index():
-    data = json.loads(request.data)
-    # Вконтакте в своих запросах всегда отправляет поле типа
-    if 'type' not in data.keys():
-        return 'not vk'
-    if data['type'] == 'confirmation':
-        return '5c003175'
-    return 'index.html'
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+        if event.text == 'Первый вариант фразы' or event.text == 'Второй вариант фразы':  # Если написали заданную фразу
+            if event.from_user:  # Если написали в ЛС
+                vk.messages.send(  # Отправляем сообщение
+                    user_id=event.user_id,
+                    message='Ваш текст',
+                    random_id=0
+                )
+            elif event.from_chat:  # Если написали в Беседе
+                vk.messages.send(  # Отправляем собщение
+                    chat_id=event.chat_id,
+                    message='Ваш текст',
+                    random_id=0
+                )
+    elif event.type == VkEventType.MESSAGE_EDIT:
+        vk.messages.send(  # Отправляем сообщение
+            user_id=event.user_id,
+            message='Изменил сообщение',
+            random_id=0
+        )
+    elif event.type == VkEventType.USER_TYPING:
+        vk.messages.send(  # Отправляем сообщение
+            user_id=event.user_id,
+            message='Че печатаешь?',
+            random_id=0
+        )
